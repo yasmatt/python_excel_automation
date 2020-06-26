c5927a3a1f71c3d68a58d2ace5962eb069ad96ae
yasmatt <yasumasa.takemura@lsmechanism.com>
2020-06-26 17:12:50 +0900

    fix params

--- million_records/main.py
+++ million_records/main.py
@@ -3,8 +3,8 @@ from  data_generator.xls import create_xls_from_csv
 
 
 if '__main__' == __name__:
-    output_csv = 'sales_data.csv'
-    output_xls = 'sales_data.xlsx'
+    output_csv = 'sales_data_m.csv'
+    output_xls = 'sales_data_m.xlsx'
     sheet_name = '売上実績'
-    generate_romdom_dataset(output_csv, num_rows=1000)
+    generate_romdom_dataset(output_csv, num_rows=1_000_000)
     create_xls_from_csv(sheet_name, output_csv, output_xls)
